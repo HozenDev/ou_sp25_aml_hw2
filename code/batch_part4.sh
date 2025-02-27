b2e66e70ef58dfc321db9ddeea81e3a0d54e9611
Course Account <cs504305@schooner3.oscer.ou.edu>
2025-02-27 16:44:32 -0600

    a

--- code/batch_part4.sh
+++ code/batch_part4.sh
@@ -17,7 +17,7 @@
 #SBATCH --mail-user=Enzo.B.Durel-1@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504305/hw2/code
-#SBATCH --array=414
+#SBATCH --array=316,405-449
 #
 #################################################
 # Do not change this line unless you have your own python/tensorflow/keras set up
